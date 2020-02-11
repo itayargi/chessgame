bf2c0fd7d3a30b5352c60be4510315aae79521d1
itayargi <itayhaza@gmail.com>
2020-02-12 01:49:45 +0200

    Updates

---
+++ precache-manifest.5ce05417b6ac84f3cf2da2f43971a55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf97c9703e7b3176da43b79265b8343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ec2b7af1892ae0d45e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4445c8a37ffe29164b25",
+    "url": "/static/js/2.9ba41f29.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.9ba41f29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9ec2b7af1892ae0d45e",
+    "url": "/static/js/main.799cfff5.chunk.js"
+  },
+  {
+    "revision": "183bdabdec7dc54d749f",
+    "url": "/static/js/runtime-main.544078fa.js"
+  }
+]);
